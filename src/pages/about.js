6487f33953b53ff75ca572cf8286dc900d76d3e3
Blake McCarty <blake_mccarty@yahoo.com>
2022-07-22 16:49:39 -0500

    finished gatsby tut part 2

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../layout/Layout";
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle={"About Page"}>
+      <p>
+        Hi there! I'm the proud creator of this site, which I built with Gatsby.
+      </p>
+    </Layout>
+  );
+};
+
+export default AboutPage;
